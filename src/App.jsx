abd468d2146faaf4ac52a02c684efa3e996ff464
Richard-Mro <mololuwa.olorunsola@gmail.com>
2023-10-04 16:37:55 +0100

    2 files modified

--- src/App.jsx
+++ src/App.jsx
@@ -7,38 +7,37 @@ function App() {
   const editInputRef = useRef(null);
 
   useEffect(() => {
-    // Fetch posts from JSONPlaceholder API
+   
     axios.get('https://jsonplaceholder.typicode.com/posts')
       .then(response => setPosts(response.data))
       .catch(error => console.error('Error fetching posts:', error));
   }, []);
 
   const handleEditClick = (postId, postTitle) => {
-    // Set the post to be edited
+   
     setEditedPost({ id: postId, title: postTitle });
     editInputRef.current.focus();
     editInputRef.current.scrollIntoView({ behavior: 'smooth', block: 'start' });
   };
 
   const handleUpdatePost = () => {
-    // Send PUT request to update the post by ID
+    
     axios.put(`https://jsonplaceholder.typicode.com/posts/${editedPost.id}`, {
       id: editedPost.id,
       title: editedPost.title,
     })
     .then(response => {
-      // Update the posts list after successful edit
       setPosts(posts.map(post => (post.id === editedPost.id ? response.data : post)));
-      setEditedPost({ id: null, title: '' }); // Reset edited post state
+      setEditedPost({ id: null, title: '' });
     })
     .catch(error => console.error('Error updating post:', error));
   };
 
   const handleDeletePost = (postId) => {
-    // Send DELETE request to delete the post by ID
+    
     axios.delete(`https://jsonplaceholder.typicode.com/posts/${postId}`)
     .then(() => {
-      // Remove the post from the posts list after successful delete
+     
       setPosts(posts.filter(post => post.id !== postId));
     })
     .catch(error => console.error('Error deleting post:', error));
